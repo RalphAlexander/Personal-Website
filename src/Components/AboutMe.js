3de16ac3a61f04ae7134b59f2adf2148184a8f46
Ralph Alexander <ralph.halim103@gmail.com>
2022-10-16 16:53:09 -0700

    Updates

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -8,19 +8,19 @@ export default function AboutMe() {
                 <img 
                     src={process.env.PUBLIC_URL +'/ProfileImageSource.jpg'}
                     className='about-me-img'/>
-            </div>
-            <div>
-                <h3 class='about-me-header'>
-                    About Me
-                </h3>                   
-                <p className='about-me-body'>
-                    I'm a student in Shoreline Community College pursuing a 
-                    major in Computer Science. I've always love gaming and that interest 
-                    shifted towards programing and building programs from the very basics 
-                    such as a simple calculator and rock, paper, scissors to more complex 
-                    programs and algorithms such as a sorting visualizer and a Sudoku Solver.
-                </p>
-            </div>
+            </div>        
+            <p className='about-me-subhead'>
+                Hey, I'm Ralph Alexander.
+            </p>                   
+            <p className='about-me-body'>
+                I'm a student in Shoreline Community College pursuing a 
+                major in Computer Science. I've always love gaming and that interest 
+                shifted towards programing and building programs from the very basics 
+                such as a simple calculator and rock, paper, scissors to more complex 
+                programs and algorithms such as a sorting visualizer and a Sudoku Solver.
+                In the midst of building projects, I eventually learnt web development
+                from basic HTML and CSS eventually leading to frameworks such as ReactJS.
+            </p>
         </div>
     </>
   )
